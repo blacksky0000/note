595eef625cce1faa018705c026576517f6a202af
KingNo21 <KingNo21.local>
2016-06-10 14:45:54 +0900

    mode

---
+++ setup/virtualenv_setup.sh
@@ -0,0 +1,11 @@
+// install python virtualenv
+
+// for ubuntu need python
+sudo apt-get install -y python python-pip python2.7-dev
+
+// for both.
+sudo pip install virtualenv virtualenvwrapper
+
+//addition.
+export WORKON_HOME=""
+source ~/.virtualenvs
